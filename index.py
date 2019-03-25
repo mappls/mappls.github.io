6e5e391ffdb9765965857a406f8a6d5033d03394
Mihajlo <m.pavloski07@gmail.com>
2019-03-25 11:41:32 +0000

    Try static

--- index.py
+++ index.py
@@ -1,10 +1,28 @@
 
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Hi"
+def get_projects_json(filepath="static/projects/projects.json"):
+    with open(filepath, "r") as file:
+        pl = json.load(file)
+    return pl
 
+
+@app.route('/')
+def get_index():
+    pl = get_projects_json()
+    return render_template('projects.html', projects=pl)
+
+
+@app.route('/projects')
+def get_projects():
+    pl = get_projects_json()
+    return render_template('projects.html', projects=pl)
+
+
+@app.route('/research')
+def get_research():
+    return "Research"
